Fixed Scaling Issue

boards/ZCU111/rfsoc_sam/notebooks/bandwidth_selector.py
@@ -28,7 +28,7 @@ class BandwidthSelector(DefaultHierarchy):
         self.buf_config.close()
 
     def reload(self, data):
-        np.copyto(self.buf_reload,np.int16(data*2**14))
+        np.copyto(self.buf_reload,np.int16(data*2**15))
         self.axi_dma_reload.sendchannel.transfer(self.buf_reload)
         self.axi_dma_reload.sendchannel.wait()
         self.buf_reload.close()
@@ -51,7 +51,7 @@ class BandwidthSelector(DefaultHierarchy):
         fig = plt.figure()
         ax = fig.add_subplot(111)
         ax.plot(0.5*fs*w/np.pi, 20*np.log10(np.abs(h)))
-        ax.set_ylim(-40, 5)
+        #ax.set_ylim(-40, 5)
         ax.set_xlim(0, 0.5*fs)
         ax.grid(True)
         ax.set_xlabel('Frequency (Hz)')
@@ -74,7 +74,7 @@ class BandwidthSelectorCore(DefaultIP):
     def __init__(self,description):
         super().__init__(description=description)
 
-    bindto = ["UoS:RFSoC:bandwidthselector:0.3.0"]
+    bindto = ["UoS:RFSoC:bandwidthselector:0.3.1"]
 
 # LUT of property addresses for our data-driven properties
 _bandSelect_props = [("downsample_factor", 4), ("event_missing", 8), ("event_unexpected", 12), ("enable", 0)]

boards/ZCU111/rfsoc_sam/notebooks/spec_plot.py
@@ -21,8 +21,8 @@ class SpecPlot():
                  debug=False,
                  colourscale='jet',
                  time=30,
-                 zmin=-50,
-                 zmax=20,
+                 zmin=-120,
+                 zmax=0,
                  dark_theme=False):
         
         
@@ -32,8 +32,8 @@ class SpecPlot():
         self.animation_period = animation_period
         self.animation_period_range = animation_period_range
         self._data = data[::-1]
-        self._data[1024] = np.mean([self._data[1023], self._data[1025]])
-        self._data[1023] = np.mean([self._data[1022], self._data[1024]])
+        #self._data[1024] = np.mean([self._data[1023], self._data[1025]])
+        #self._data[1023] = np.mean([self._data[1022], self._data[1024]])
         self._z = np.empty((self._time,int(2048/4)))
         self._z[:] = np.nan
         frameTime = 2048/self._Fs * 1e6
@@ -69,11 +69,11 @@ class SpecPlot():
                 'height': np.ceil(h*0.4),
                 'autosize': True,
                 'xaxis': {
-                    'showticklabels' : False,
+                    'showticklabels' : True,
                 },
                 'yaxis': {
-                    'title': 'Power Spectral Density (dB/Hz)',
-                    'range': [-60, 20]
+                    'title': 'Power Spectrum (dBm/Hz)',
+                    'range': [-140, 0]
                 },
                 'margin': {
                     't':0,
@@ -86,7 +86,7 @@ class SpecPlot():
         
         layout_spectrogram={
                 'hovermode': 'closest',
-                'height':  np.ceil(h*0.6),
+                'height':  np.ceil(h*0.4),
                 'autosize': True,
                 'xaxis': {
                     'autorange' : False,
@@ -95,7 +95,7 @@ class SpecPlot():
                       max(self._x_data_spectrogram)]
                 },
                 'yaxis': {
-                     'title': 'Time (us)',
+                     'title': 'Frame Number',
                 },
                 'margin': {
                     't':0,
@@ -122,7 +122,7 @@ class SpecPlot():
                                         visible=False))
         
         # 3D Spectrogram Plot
-        trace = go.Heatmap(x=self._x_data_spectrogram, y=self._y, z=self._z, colorscale = colourscale, zmin=-60, zmax=20,showscale=False,zsmooth='fast')
+        trace = go.Heatmap(x=self._x_data_spectrogram, y=self._y, z=self._z, colorscale = colourscale, zmin=-120, zmax=0,showscale=False,zsmooth='fast')
         self._plot_spectrogram = go.FigureWidget(
             layout=layout_spectrogram,
             data=[trace])
@@ -144,8 +144,8 @@ class SpecPlot():
         frame (list): new time domain samples to FFT and add to the plot"""
         
         self._data = frame[::-1]
-        self._data[1024] = np.mean([self._data[1023], self._data[1025]])
-        self._data[1023] = np.mean([self._data[1022], self._data[1024]])
+        #self._data[1024] = np.mean([self._data[1023], self._data[1025]])
+        #self._data[1023] = np.mean([self._data[1022], self._data[1024]])
         
         self._avg_window = np.roll(self._avg_window, 1, axis=0)
         self._avg_window[0] = self._data

boards/ZCU111/rfsoc_sam/notebooks/spectrum_window.py
@@ -19,7 +19,7 @@ class SpectrumWindow(DefaultHierarchy):
         self.set_window_coeffs(np.ones(2048))
         self.set_enable(1)
         
-        self.window_0.dc_enable = 1
+        self.window.dc_enable = 1
 
         self.params = ipw.VBox([],layout=ipw.Layout(width='auto'))
         self.window_sel = ipw.HBox([],layout=ipw.Layout(width='auto'))
@@ -28,12 +28,13 @@ class SpectrumWindow(DefaultHierarchy):
         self.window_type = 'Rectangular'
 
         self.coeffs_sat = np.ones(2048)
+        self.scale_factor = 2048
 
         self.frequency = 0
         self.input = 0
         
     def set_enable(self, enable):
-        self.window_0.enable = int(enable)
+        self.window.enable = int(enable)
     
     def set_window_coeffs(self, data):
         coeffs = np.int16(data*2**14)
@@ -49,6 +50,7 @@ class SpectrumWindow(DefaultHierarchy):
         np.copyto(self.input_buffer,window_coeffs)
         dma.sendchannel.transfer(self.input_buffer)
         dma.sendchannel.wait()
+        self.scale_factor = np.sum(self.coeffs**2)
         self.input_buffer.close()
 
     def updateParameters(self, update): 
@@ -120,7 +122,7 @@ class SpectrumWindow(DefaultHierarchy):
     @staticmethod
     def checkhierarchy(description):
         if 'axi_dma_window' in description['ip'] \
-           and 'window_0' in description['ip']:
+           and 'window' in description['ip']:
             return True
         return False  
 
@@ -132,7 +134,7 @@ class WindowCore(DefaultIP):
     def __init__(self,description):
         super().__init__(description=description)
         
-    bindto = ["UoS:RFSoC:window:0.4"]
+    bindto = ["xilinx.com:ip:window:1.0"]
     
 # LUT of property addresses for our data-driven properties
 _window_props = [("dc_enable", 0x104), ("enable", 0x100)]