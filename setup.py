7eabcd6f56ed0bec230efe8cbb76d1478ce322dd
David Northcote <42978178+dnorthcote@users.noreply.github.com>
2021-01-29 17:12:16 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ board = os.environ['BOARD']
 repo_board_folder = f'boards/{board}/rfsoc_sam'
 board_notebooks_dir = os.environ['PYNQ_JUPYTER_NOTEBOOKS']
 board_project_dir = os.path.join(board_notebooks_dir, 'spectrum_analyser')
+hw_data_files = []
+assets = []
 
 # check whether board is supported
 def check_env():
@@ -28,6 +30,16 @@ def copy_overlays():
     src_ol_dir = os.path.join(repo_board_folder, 'bitstream')
     dst_ol_dir = os.path.join('rfsoc_sam', 'bitstream')
     copy_tree(src_ol_dir, dst_ol_dir)
+    hw_data_files.extend(
+        [os.path.join("..", dst_ol_dir, f) for f in os.listdir(dst_ol_dir)])
+
+# copy assets to python package
+def copy_assets():
+    src_at_dir = os.path.join(repo_board_folder, 'notebooks', 'assets')
+    dst_at_dir = os.path.join('rfsoc_sam', 'assets')
+    copy_tree(src_at_dir, dst_at_dir)
+    assets.extend(
+        [os.path.join("..", dst_at_dir, f) for f in os.listdir(dst_at_dir)])
 
 # copy board specific drivers
 def copy_drivers():
@@ -44,6 +56,7 @@ def copy_notebooks():
 check_env()
 check_path()
 copy_overlays()
+copy_assets()
 copy_drivers()
 copy_notebooks()
 
@@ -58,4 +71,8 @@ setup(
     author="Lewis Davin McLaughlin",
     author_email="lewis.mclaughlin@strath.ac.uk",
     packages=find_packages(),
+    package_data={
+        '': hw_data_files,
+        '': assets,
+    }
     description="PYNQ example of using the RFSoC as a Spectrum Analyser.")
