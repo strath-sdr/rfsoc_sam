03627db6cd0fb86bb5242c62e7fa344b81b3285c
David Northcote <42978178+dnorthcote@users.noreply.github.com>
2021-02-01 22:53:30 +0000

    Notebooks to be included in pip package

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ from distutils.dir_util import copy_tree
 from setuptools import find_packages, setup
 
 # global variables
+package_name = 'rfsoc_sam'
 board = os.environ['BOARD']
 repo_board_folder = f'boards/{board}/rfsoc_sam'
 board_notebooks_dir = os.environ['PYNQ_JUPYTER_NOTEBOOKS']
-board_project_dir = os.path.join(board_notebooks_dir, 'spectrum_analyser')
+board_project_dir = os.path.join(board_notebooks_dir, package_name)
+
 data_files = []
 
 # check whether board is supported
@@ -27,7 +29,7 @@ def check_path():
 # copy overlays to python package
 def copy_overlays():
     src_ol_dir = os.path.join(repo_board_folder, 'bitstream')
-    dst_ol_dir = os.path.join('rfsoc_sam', 'bitstream')
+    dst_ol_dir = os.path.join(package_name, 'bitstream')
     copy_tree(src_ol_dir, dst_ol_dir)
     data_files.extend(
         [os.path.join("..", dst_ol_dir, f) for f in os.listdir(dst_ol_dir)])
@@ -35,7 +37,7 @@ def copy_overlays():
 # copy assets to python package
 def copy_assets():
     src_at_dir = os.path.join(repo_board_folder, 'assets')
-    dst_at_dir = os.path.join('rfsoc_sam', 'assets')
+    dst_at_dir = os.path.join(package_name, 'assets')
     copy_tree(src_at_dir, dst_at_dir)
     data_files.extend(
         [os.path.join("..", dst_at_dir, f) for f in os.listdir(dst_at_dir)])
@@ -43,7 +45,7 @@ def copy_assets():
 # copy board specific drivers
 def copy_drivers():
     src_dr_dir = os.path.join(repo_board_folder, 'drivers')
-    dst_dr_dir = os.path.join('rfsoc_sam')
+    dst_dr_dir = os.path.join(package_name)
     copy_tree(src_dr_dir, dst_dr_dir)
 
 # copy notebooks to jupyter home
@@ -52,15 +54,24 @@ def copy_notebooks():
     dst_nb_dir = os.path.join(board_project_dir)
     copy_tree(src_nb_dir, dst_nb_dir)
     
+# copy notebooks to python package
+def copy_notebooks_package():
+    src_nb_dir = os.path.join(repo_board_folder, 'notebooks')
+    dst_nb_dir = os.path.join(package_name, 'notebooks')
+    copy_tree(src_nb_dir, dst_nb_dir)
+    data_files.extend(
+        [os.path.join("..", dst_nb_dir, f) for f in os.listdir(dst_nb_dir)])
+    
 check_env()
 check_path()
 copy_overlays()
 copy_assets()
 copy_drivers()
 copy_notebooks()
+copy_notebooks_package()
 
 setup(
-    name="rfsoc_sam",
+    name=package_name,
     version='0.2',
     install_requires=[
         'pynq>=2.6',
