81157a11cad95a1f4c9a4c05a6bead24c21c8b70
David Northcote <42978178+dnorthcote@users.noreply.github.com>
2021-03-10 10:45:46 +0000

    Do not update if spectrum contains NaN

--- rfsoc_sam/sdr_plots.py
+++ rfsoc_sam/sdr_plots.py
@@ -157,28 +157,30 @@ class Spectrum():
     def data(self, data):
         
         if self.enable_updates:
+
+            if not any(np.isnan(data)):
         
-            def maximum_hold(old_data, new_data):
-                return (old_data > new_data) * old_data + (old_data < new_data) * new_data
+                def maximum_hold(old_data, new_data):
+                    return (old_data > new_data) * old_data + (old_data < new_data) * new_data
 
-            if self._spectrum_mode:
-                fdata = np.fft.fftshift(data)
-            else:
-                fdata = data
+                if self._spectrum_mode:
+                    fdata = np.fft.fftshift(data)
+                else:
+                    fdata = data
 
-            if self._display_mode == 0:
-                self._y_data = fdata[int(np.ceil((self._number_samples/2)*(1-self._nyquist_stopband))) \
-                                   :int(self._number_samples - int(np.ceil((self._number_samples/2)*(1-self._nyquist_stopband))))]
-            elif self._display_mode == 1:
-                self._y_data = fdata[int(np.ceil((self._number_samples/2)*(1-self._nyquist_stopband))) \
-                                   :int(self._number_samples/2)]
-            else:
-                pass
+                if self._display_mode == 0:
+                    self._y_data = fdata[int(np.ceil((self._number_samples/2)*(1-self._nyquist_stopband))) \
+                                    :int(self._number_samples - int(np.ceil((self._number_samples/2)*(1-self._nyquist_stopband))))]
+                elif self._display_mode == 1:
+                    self._y_data = fdata[int(np.ceil((self._number_samples/2)*(1-self._nyquist_stopband))) \
+                                    :int(self._number_samples/2)]
+                else:
+                    pass
 
-            if self.post_process == 'max':
-                self._y_data = maximum_hold(self._plot.data[0].y, self._y_data)
+                if self.post_process == 'max':
+                    self._y_data = maximum_hold(self._plot.data[0].y, self._y_data)
 
-            self._plot.data[0].update({'x':self._x_data, 'y':self._y_data})
+                self._plot.data[0].update({'x':self._x_data, 'y':self._y_data})
     
     @property
     def xlabel(self):
