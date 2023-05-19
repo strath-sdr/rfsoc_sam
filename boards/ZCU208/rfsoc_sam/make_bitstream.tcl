e56452858c37251155ce1d3b3f3b1633efd3f330
David Northcote <42978178+dnorthcote@users.noreply.github.com>
2023-05-19 07:41:33 +0100

    add ZCU208

---
+++ boards/ZCU208/rfsoc_sam/make_bitstream.tcl
@@ -0,0 +1,26 @@
+set overlay_name "block_design"
+set design_name "rfsoc_sam"
+
+# Open project
+open_project ./${overlay_name}/${overlay_name}.xpr
+open_bd_design ./${overlay_name}/${overlay_name}.srcs/sources_1/bd/${design_name}/${design_name}.bd
+
+# Add top wrapper and xdc files
+make_wrapper -files [get_files ./${overlay_name}/${overlay_name}.srcs/sources_1/bd/${design_name}/${design_name}.bd] -top
+add_files -norecurse ./${overlay_name}/${overlay_name}.gen/sources_1/bd/${design_name}/hdl/${design_name}_wrapper.vhd
+set_property top ${design_name}_wrapper [current_fileset]
+update_compile_order -fileset sources_1
+
+# Set strategy
+set_property strategy Performance_ExplorePostRoutePhysOpt [get_runs impl_1]
+
+# Call implement
+launch_runs impl_1 -to_step write_bitstream -jobs 8
+wait_on_run impl_1
+
+# Move and rename bitstream to final location
+file mkdir ./bitstream
+file copy -force ./${overlay_name}/${overlay_name}.runs/impl_1/${design_name}_wrapper.bit ./bitstream/${design_name}.bit
+
+# copy hwh files
+file copy -force ./${overlay_name}/${overlay_name}.gen/sources_1/bd/${design_name}/hw_handoff/${design_name}.hwh ./bitstream/${design_name}.hwh
